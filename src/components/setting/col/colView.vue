a47cf37c1a3905f82ca2c35c7c0d102bb76abb4e
程序猿 <312660145@qq.com>
2024-06-07 12:41:40 +0800

    -

--- src/components/setting/col/colView.vue
+++ src/components/setting/col/colView.vue
@@ -1,6 +1,22 @@
 <template>
   <a-divider>常规属性</a-divider>
   {{ element }}
+  <a-form 
+    :model="element"
+    :label-col="{ span: 6 }"
+    :wrapper-col="{ span: 16 }"
+    autocomplete="off"
+    labelAlign="left"
+  >
+    <a-form-item label="隐藏" name="hidden" >
+      <a-switch v-model:checked="element.options.hidden" />
+    </a-form-item>
+    <a-form-item label="高度" name="colHeight" >
+      <a-input-number v-model:value="element.options.colHeight" :min="50" />
+    </a-form-item>
+    <a-divider>栅格属性</a-divider>
+    
+  </a-form>
 </template>
 
 <script setup>
